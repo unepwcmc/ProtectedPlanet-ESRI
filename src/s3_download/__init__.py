ab2ef6bc31a240f0eba25ee121396a8de3ef5e8a
Miguel Torres <miguel.torres@unep-wcmc.org>
2014-05-27 12:11:40 +0000

    Script now downloads file to backup directory

--- src/s3_download/__init__.py
+++ src/s3_download/__init__.py
@@ -5,18 +5,15 @@ import yaml
 s3_secrets_file = open(os.path.join('src', 'config','s3.yaml'),'r')
 s3_secrets = yaml.load(s3_secrets_file)
 
-LOCAL_PATH = 'd:\\backup\\s3\\'
+
+FILE_PATH = 'd:\\backup\\s3\\current_wdpa.zip'
 AWS_ACCESS_KEY_ID = s3_secrets['access_key_id']
 AWS_SECRET_ACCESS_KEY = s3_secrets['secret_access_key']
-
-bucket_name = 'wdpa'
+bucket_name = s3_secrets['bucket_name']
 # connect to the bucket
 conn = boto.connect_s3(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY)
 bucket = conn.get_bucket(bucket_name)
 # go through the list of files
-bucket_list = bucket.list()
-for l in bucket_list:
-    keyString = str(l.key)
-    # check if file exists locally, if not: download it
-    if not os.path.exists(LOCAL_PATH+keyString):
-        l.get_contents_to_filename(LOCAL_PATH+keyString)
+file_list = [(k.last_modified, k) for k in bucket]
+key_to_download = sorted(file_list, cmp=lambda x,y: cmp(x[0], y[0]))[-1][1]
+key_to_download.get_contents_to_filename(FILE_PATH)
