b1d6e35c59cf772f0f387b9f53950a1d13f3f7b7
Miguel Torres <miguel.torres@unep-wcmc.org>
2014-05-30 15:41:16 +0000

    Changes old filegdb to _old and moves new to right place

--- src/update_service/update_files.py
+++ src/update_service/update_files.py
@@ -1,11 +1,18 @@
 import os
+
 class UpdateFiles:
 
     def __init__ (self):
         filepath='d:\\backup\\s3\\'
+        target_directory = 'd:\\data\\'
+        filegdb_name = 'WDPA_current.gdb'
+        old_suffix = '_old'
         all_subdirs = self.all_subdirs_of(filepath)
         latest_subdir = max(all_subdirs, key=os.path.getmtime)
-        return latest_subdir
+        old_filegdb_name = self.all_subdirs_of(latest_subdir)[0]
+        os.renames(target_directory + filegdb_name, target_directory + filegdb_name + old_suffix)
+        os.renames(old_filegdb_name, target_directory + filegdb_name)
+
 
     def all_subdirs_of(self, b='.'):
         result = []
@@ -13,3 +20,7 @@ class UpdateFiles:
             bd = os.path.join(b, d)
             if os.path.isdir(bd): result.append(bd)
         return result
+
+
+
+
