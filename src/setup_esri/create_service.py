e7681942715f1257269308922ebaafd3e21a0ee7
Miguel Torres <miguel.torres@unep-wcmc.org>
2014-05-30 10:59:17 +0000

    Cleaned comments

--- src/setup_esri/create_service.py
+++ src/setup_esri/create_service.py
@@ -1,6 +1,5 @@
 import arcpy
 
-# define local variables
 workspace = 'd:/projects/ProtectedPlanet-ESRI/lib/'
 mapDoc = arcpy.mapping.MapDocument(workspace + 'wdpa.mxd')
 con = 'GIS Servers/arcgis on localhost_6080 (admin).ags'
@@ -10,15 +9,9 @@ sd = workspace + service + '.sd'
 summary = 'WDPA'
 tags = 'Protected Areas, Conservation, UNEP-WCMC'
 
-# create service definition draft
-analysis = analysis = arcpy.mapping.AnalyzeForSD(sddraft)
-
-# stage and upload the service if the sddraft analysis did not contain errors
+# Analyse service definition draft
+analysis = arcpy.mapping.AnalyzeForSD(sddraft)
 if analysis['errors'] == {}:
-    # Execute StageService
-    arcpy.StageService_server(sddraft, sd)
-    # Execute UploadServiceDefinition
     arcpy.UploadServiceDefinition_server(sd, con)
 else:
-    # if the sddraft analysis contained errors, display them
     print analysis['errors']
