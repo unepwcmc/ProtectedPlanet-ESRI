88a230134eec69e5bf217b07a5a20fc480a0aa31
Miguel Torres <miguel.torres@unep-wcmc.org>
2014-06-02 18:12:26 +0000

    update class from beginning to end

--- src/s3_download/__init__.py
+++ src/s3_download/__init__.py
@@ -8,7 +8,7 @@ s3_secrets_file = open(os.path.join('src', 'config','s3.yaml'),'r')
 s3_secrets = yaml.load(s3_secrets_file)
 
 
-FILE_PATH = 'd:\\backup\\s3\\current_wdpa.zip'
+FILE_PATH = 'd:\\data\\s3\\current_wdpa.zip'
 AWS_ACCESS_KEY_ID = s3_secrets['access_key_id']
 AWS_SECRET_ACCESS_KEY = s3_secrets['secret_access_key']
 bucket_name = s3_secrets['bucket_name']
@@ -21,4 +21,4 @@ file_list = [(k.last_modified, k) for k in bucket]
 key_to_download = sorted(file_list, cmp=lambda x,y: cmp(x[0], y[0]))[-1][1]
 key_to_download.get_contents_to_filename(FILE_PATH)
 zip_file = ZipFile(FILE_PATH)
-zip_file.extractall('d:\\backup\\s3\\')
+zip_file.extractall('d:\\data\\s3\\')
--- src/update_service/update_files.py
+++ src/update_service/update_files.py
@@ -1,17 +1,29 @@
 import os
+import arcpy
+from arcpy import env
+
 
 class UpdateFiles:
 
     def __init__ (self):
-        filepath='d:\\backup\\s3\\'
+
         target_directory = 'd:\\data\\'
-        filegdb_name = 'WDPA_current.gdb'
+        download_path = target_directory + 's3\\'
+        current_filegdb_name = 'WDPA_current.gdb'
         old_suffix = '_old'
-        all_subdirs = self.all_subdirs_of(filepath)
-        latest_subdir = max(all_subdirs, key=os.path.getmtime)
-        old_filegdb_name = self.all_subdirs_of(latest_subdir)[0]
-        os.renames(target_directory + filegdb_name, target_directory + filegdb_name + old_suffix)
-        os.renames(old_filegdb_name, target_directory + filegdb_name)
+
+        download_subdirs = self.all_subdirs_of(download_path)
+        latest_download = self.latest_subdir(download_subdirs)
+        filegdb_name = self.all_subdirs_of(latest_download)[0]
+        env.workspace = download_path + filegdb_name
+        poly_table = self.feature_name('WDPA_poly')
+        point_table = self.feature_name('WDPA_point')
+        self.change_table_name(poly_table, 'poly')
+        self.change_table_name(point_table, 'point')
+
+        os.renames(target_directory + current_filegdb_name, target_directory + current_filegdb_name + old_suffix)
+        os.renames(download_path + filegdb_name, target_directory + filegdb_name)
+
 
 
     def all_subdirs_of(self, b='.'):
@@ -20,6 +32,23 @@ class UpdateFiles:
             bd = os.path.join(b, d)
             if os.path.isdir(bd): result.append(bd)
         return result
+
+    def latest_subdir(self, all_subdirs):
+        return max(all_subdirs, key=os.path.getmtime)
+
+
+    def feature_name(self, name):
+
+        feature_classes = arcpy.ListFeatureClasses()
+        debugger
+        print name
+        return filter(lambda x:name in x, feature_classes)[0]
+
+    def change_table_name(self, table, geometry_type):
+        return arcpy.Rename_management(table, 'WDPA_' + geometry_type)
+
+
+
 
 
 
