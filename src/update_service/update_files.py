6d158811c5e95144e062ffb335e34b1bf6256020
Miguel Torres <miguel.torres@unep-wcmc.org>
2014-06-03 17:36:32 +0000

    Fixed bugs, now fully working update_files.py

--- src/update_service/update_files.py
+++ src/update_service/update_files.py
@@ -12,39 +12,45 @@ class UpdateFiles:
         current_filegdb_name = 'WDPA_current.gdb'
         old_suffix = '_old'
 
-        download_subdirs = self.all_subdirs_of(download_path)
+        download_subdirs = self.all_subdirs_of(download_path, 'true')
         latest_download = self.latest_subdir(download_subdirs)
-        filegdb_name = self.all_subdirs_of(latest_download)[0]
-        env.workspace = download_path + filegdb_name
-        poly_table = self.feature_name('WDPA_poly')
-        point_table = self.feature_name('WDPA_point')
+        filegdb_full_path = self.all_subdirs_of(latest_download, 'true')[0]
+        filegdb_name = self.all_subdirs_of(latest_download, 'false')[0]
+        print filegdb_full_path
+        print filegdb_name
+        env.workspace = filegdb_full_path
+        poly_table = self.feature_name('WDPA_poly')[0]
+        point_table = self.feature_name('WDPA_point')[0]
         self.change_table_name(poly_table, 'poly')
         self.change_table_name(point_table, 'point')
 
         os.renames(target_directory + current_filegdb_name, target_directory + current_filegdb_name + old_suffix)
-        os.renames(download_path + filegdb_name, target_directory + filegdb_name)
+        os.renames(filegdb_full_path, target_directory + current_filegdb_name)
 
 
 
-    def all_subdirs_of(self, b='.'):
-        result = []
-        for d in os.listdir(b):
-            bd = os.path.join(b, d)
-            if os.path.isdir(bd): result.append(bd)
-        return result
+    def all_subdirs_of(self,directory, is_full_path):
+        return self.full_subdirs_path(directory) if is_full_path == 'true' else self.small_subdirs_path(directory)
+
+    def full_subdirs_path(self, directory):
+        return [os.path.join(directory, name) for name in os.listdir(directory)
+            if os.path.isdir(os.path.join(directory, name))]
+
+    def small_subdirs_path(self, directory):
+        return [name for name in os.listdir(directory)
+            if os.path.isdir(os.path.join(directory, name))]
+
 
     def latest_subdir(self, all_subdirs):
         return max(all_subdirs, key=os.path.getmtime)
 
 
     def feature_name(self, name):
-
         feature_classes = arcpy.ListFeatureClasses()
-        debugger
-        print name
-        return filter(lambda x:name in x, feature_classes)[0]
+        return filter(lambda x:name in x, feature_classes)
 
     def change_table_name(self, table, geometry_type):
+        print table
         return arcpy.Rename_management(table, 'WDPA_' + geometry_type)
 
 
